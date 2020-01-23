e1b2097f810c21e5d0e3bbfbdef88ef1f5438489
egan-seamus <egan.seam@husky.neu.edu>
2020-01-23 12:28:40 -0500

    added some stuff idk man

---
+++ a1datatypes.h
@@ -0,0 +1,17 @@
+#ifndef A1DATATYPES_H
+#define A1DATATYPES_H
+
+#include <string>
+//this file defines functions to check if data of of the data types
+//as defined in assignment one
+
+/**
+ * @brief determine if the given data is of string type
+ * @note data will be given as it is in the file but without the <> separators
+ */ 
+int isString(std::string data)
+{
+
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,20 +1,42 @@
 #include <iostream>
 #include <cstdio>
+#include <string>
+#include <fstream>
+
 
 void readFile(const char *filename)
 {
-    FILE *myFile = fopen(filename, "r");
-    char readFromFile [300];
-    if (myFile == NULL)
-    {
-        std::cout << "bad file name\n";
-        exit(1);
-    }
+    std::ifstream myFile;
+    std::string fileInput;
+    myFile.open(filename);
 
-    if(fgets (readFromFile, 300, myFile) != NULL)
+    while (myFile)
     {
-        std::cout << readFromFile << "\n";
+        std::getline(myFile, fileInput);
+        std::cout<<fileInput<<"\n";
     }
+}
+
+/**
+ * @brief determine the data type of this row
+ * @note if any value in the row is a string, then the row is a string
+ * @note else if any value is a float then the row is a float
+ * @note else if any value is an int then the row is an int
+ * @note else the row is bool
+ */ 
+void determineDataType(std::string row)
+{
+
+}
+
+/**
+ * @brief get the value of the given row at the given column
+ * @param row the string representing the data at that row
+ * @param column the column to target
+*/
+void getColumn(std::string row, size_t column)
+{
+
 }
 
 int main(int argc, char **argv)
