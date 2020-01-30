47ca19004bffffe47f9de1ebbf84e9698f383857
Jeremy Eng <jeremy@jeremyeng.com>
2020-01-29 19:47:34 -0500

    Revert "remove string-array import"
    This reverts commit 3e5bc772c78cc59203f8cf7ba17698890737b8df.

--- part2/test.cpp
+++ part2/test.cpp
@@ -1,4 +1,5 @@
 #include "array.h"
+#include "string-array.h"
 #include <assert.h>
 
 void testArray()
@@ -71,76 +72,6 @@ void testArray()
     array.erase();
 
     assert(array.size() == 0);
-}
-
-void testStringArray()
-{
-    String s1;
-    String s2;
-    String s3;
-
-    StringArray stringArray;
-
-    if (stringArray.pushBack(&s1) == 1)
-    {
-        assert(stringArray.size() == 1);
-        assert(stringArray.valueAt(0) == &s1);
-    }
-
-    stringArray.pushBack(&s1);
-    stringArray.pushBack(&s2);
-    stringArray.pushBack(&s3);
-
-    assert(stringArray.size() == 3);
-    assert(stringArray.valueAt(0) == &s1);
-    assert(stringArray.valueAt(1) == &s2);
-    assert(stringArray.valueAt(2) == &s3);
-
-    stringArray.popBack();
-
-    assert(stringArray.size() == 2);
-    assert(stringArray.valueAt(0) == &s1);
-    assert(stringArray.valueAt(1) == &s2);
-
-    stringArray.pushBack(&s3);
-
-    stringArray.insert(&s1, 1);
-
-    assert(stringArray.size() == 4);
-    assert(stringArray.valueAt(0) == &s1);
-    assert(stringArray.valueAt(1) == &s1);
-    assert(stringArray.valueAt(2) == &s2);
-    assert(stringArray.valueAt(3) == &s3);
-
-    stringArray.emplace(&s2, 1);
-
-    assert(stringArray.size() == 4);
-    assert(stringArray.valueAt(0) == &s1);
-    assert(stringArray.valueAt(1) == &s2);
-    assert(stringArray.valueAt(2) == &s2);
-    assert(stringArray.valueAt(3) == &s3);
-
-    stringArray.popBack();
-
-    assert(stringArray.size() == 3);
-    assert(stringArray.valueAt(0) == &s1);
-    assert(stringArray.valueAt(1) == &s2);
-    assert(stringArray.valueAt(2) == &s2);
-
-    assert(stringArray.indexOf(&s2) == 1);
-
-    stringArray.emplace(&s3, 1);
-
-    assert(stringArray.size() == 3);
-    assert(stringArray.valueAt(0) == &s1);
-    assert(stringArray.valueAt(1) == &s3);
-    assert(stringArray.valueAt(2) == &s2);
-
-    assert(stringArray.indexOf(&s2) == 2);
-
-    stringArray.erase();
-
-    assert(stringArray.size() == 0);
 }
 
 int main(int argc, char **argv)
